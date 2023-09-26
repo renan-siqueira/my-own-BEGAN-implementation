33e59d622e707a4782cc4ab92451ff2914dc9635
Renan Siqueira Antonio <renan.siqu@gmail.com>
2023-09-26 12:05:55 -0300

    feat: remove unnecessary config at settings file

--- src/config/settings.example.py
+++ src/config/settings.example.py
@@ -4,8 +4,5 @@ PATH_DATASET = "dataset"
 PATH_IMAGES_GENERATED = 'src/generated/images'
 PATH_VIDEOS_GENERATED = 'src/generated/videos'
 
-# EXTERNAL SETTINGS
-PATH_ORIGINAL_FILES = "__your_original_files_path__",
-
 # JSON FILES SETTINGS
 PATH_TRAIN_PARAMS = 'src/json/training_params.json'
