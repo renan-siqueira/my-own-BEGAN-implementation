861998598c8a3516c1bc7ed85c169664a67e0875
Renan Siqueira Antonio <renan.siqu@gmail.com>
2023-09-29 17:13:27 -0300

    feat: Pylint refactoring

--- src/app/discriminator.py
+++ src/app/discriminator.py
@@ -1,28 +1,107 @@
+"""
+Module for defining the Discriminator of a Generative Adversarial Network (GAN).
+
+This module introduces a discriminator architecture designed with an encoder-decoder structure. 
+The Discriminator aims to distinguish between real and generated images. The encoding section 
+downsamples the input image, extracting important features. The decoding section then upsamples 
+the features back into an image space. Each block in the discriminator consists of a convolution 
+or transposed convolution followed by batch normalization and a leaky ReLU activation.
+
+Classes:
+- `Discriminator`: The main class representing the discriminator model. It constructs the 
+                   discriminator architecture using the provided parameters and defines the 
+                   forward method for processing input images through the model.
+
+Dependencies:
+- torch.nn: For neural network operations and building block definitions.
+- math: For mathematical operations, particularly for dynamically determining model depth.
+
+Notes:
+- This discriminator is designed to be used with a corresponding generator in a GAN setup.
+- The encoder-decoder structure can be tailored based on specific architectural needs and 
+  the desired input image size.
+"""
 import math
 import torch.nn as nn
 
 
 class Discriminator(nn.Module):
+    """
+    Represents the Discriminator of a Generative Adversarial Network (GAN).
+    
+    The Discriminator maps input images through an encoder and decoder sequence to produce 
+    output images. The aim is to discriminate between real and fake images.
+    
+    Attributes:
+    - encoder (nn.Sequential): Encoder part of the model.
+    - decoder (nn.Sequential): Decoder part of the model.
+    
+    Methods:
+    - forward(x): Processes the input image `x` through the Discriminator.
+    - _block(...): Returns a block of layers used in the Discriminator's architecture.
+    - _make_layers(...): Creates and returns a sequence of blocks for encoding or decoding.
+    """
     def __init__(self, channels_img, features_d, img_size):
+        """
+        Initializes the Discriminator with the given parameters.
+        
+        Parameters:
+        - channels_img (int): Number of channels in the input image.
+        - features_d (int): Base number of features in the discriminator.
+        - img_size (int): Size of the input image (assumed to be square).
+        """
         super(Discriminator, self).__init__()
 
         self.encoder = self._make_layers(channels_img, features_d, img_size, mode='encoder')
         self.decoder = self._make_layers(channels_img, features_d, img_size, mode='decoder')
 
     def _block(self, in_channels, out_channels, kernel_size, stride, padding, mode='encoder'):
+        """
+        Returns a block of layers used either in the encoding or decoding parts of the 
+        Discriminator.
+        
+        Parameters:
+        - in_channels (int): Number of input channels to the block.
+        - out_channels (int): Number of output channels from the block.
+        - kernel_size (int): Size of the convolutional kernel.
+        - stride (int): Stride of the convolutional kernel.
+        - padding (int): Padding added to the input before convolution.
+        - mode (str): Either 'encoder' or 'decoder' to determine the type of block.
+
+        Returns:
+        - nn.Sequential: A block for the Discriminator model.
+        """
         layers = []
         if mode == 'encoder':
-            layers.append(nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False))
+            layers.append(
+                nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False)
+            )
             layers.append(nn.BatchNorm2d(out_channels))
             layers.append(nn.LeakyReLU(0.2, inplace=True))
             layers.append(nn.Dropout(0.25))
         elif mode == 'decoder':
-            layers.append(nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias=False))
+            layers.append(
+                nn.ConvTranspose2d(
+                    in_channels, out_channels, kernel_size, stride, padding, bias=False
+                )
+            )
             layers.append(nn.BatchNorm2d(out_channels))
             layers.append(nn.LeakyReLU(0.2, inplace=True))
         return nn.Sequential(*layers)
 
     def _make_layers(self, channels_img, features_d, img_size, mode):
+        """
+        Creates a sequence of blocks for either encoding or decoding.
+
+        Parameters:
+        - channels_img (int): Number of channels in the input image.
+        - features_d (int): Base number of features in the discriminator.
+        - img_size (int): Size of the input image (assumed to be square).
+        - mode (str): Either 'encoder' or 'decoder' to determine the type of sequence.
+
+        Returns:
+        - nn.Sequential: A sequence of blocks.
+        """
         layers = []
         in_channels = channels_img
         out_channels = features_d
@@ -46,7 +125,16 @@ class Discriminator(nn.Module):
 
         return nn.Sequential(*layers)
 
-    def forward(self, x):
-        x = self.encoder(x)
-        x = self.decoder(x)
-        return x
+    def forward(self, x_vector):
+        """
+        Processes the input image through the Discriminator.
+
+        Parameters:
+        - x_vector (torch.Tensor): Input image tensor.
+
+        Returns:
+        - torch.Tensor: Output image tensor after passing through the Discriminator.
+        """
+        x_vector = self.encoder(x_vector)
+        x_vector = self.decoder(x_vector)
+        return x_vector
--- src/modules/image_generate.py
+++ src/modules/image_generate.py
@@ -1,9 +1,38 @@
-import os
-import numpy as np
+"""
+This script is responsible for generating and saving images using a pre-trained 
+Generative Adversarial Network (GAN) model.
 
+Functions:
+- `generate_latent_points`: Generates random latent points that serve as input for 
+the generator model.
+- `generate_images`: Uses the generator model to produce images from latent points.
+- `tensor_to_pil_image`: Converts a PyTorch tensor into a PIL image.
+- `main`: Main function that loads a pre-trained generator model, generates images, 
+and saves them individually and in a grid format.
+
+Utilizes auxiliary modules for image resizing, GPU availability check, and generator 
+definitions.
+
+Dependencies:
+- os: For path and directory manipulation.
+- numpy: For mathematical operations.
+- torch and torchvision: For deep learning operations and image manipulation.
+- PIL: For image processing.
+- tqdm: To display a progress bar.
+- src.app.generator: Generator model definitions.
+- src.app.utils: Utility functions.
+- .resize_image: Functions to resize images.
+
+Notes:
+- Generated images are normalized to the range [0, 1] and can be resized to a specified width.
+- The generator model is loaded from a checkpoint and placed in evaluation mode before image 
+generation.
+"""
+import os
+
+import numpy as np
 import torch
 import torchvision.utils as vutils
-
 from PIL import Image
 from tqdm import tqdm
 
@@ -13,11 +42,35 @@ from .resize_image import process_and_resize_image
 
 
 def generate_latent_points(latent_dimension, num_samples, device):
+    """
+    Generate random latent points as input for the generator model.
+    
+    Parameters:
+    - latent_dimension (int): The size of the latent space.
+    - num_samples (int): The number of latent points to generate.
+    - device (torch.device): The device to which the generated points 
+    should be assigned (e.g., 'cuda' or 'cpu').
+
+    Returns:
+    - torch.Tensor: A tensor of randomly generated latent points.
+    """
     points = torch.randn(num_samples, latent_dimension, device=device)
     return points
 
 
 def generate_images(model, latent_dimension, num_samples, device):
+    """
+    Generate images using a pre-trained generator model.
+    
+    Parameters:
+    - model (torch.nn.Module): The pre-trained generator model.
+    - latent_dimension (int): The size of the latent space.
+    - num_samples (int): The number of images to generate.
+    - device (torch.device): The device on which the model and points are located.
+
+    Returns:
+    - torch.Tensor: A tensor of generated images.
+    """
     points = generate_latent_points(latent_dimension, num_samples, device)
     points = points.view(num_samples, latent_dimension, 1, 1)
     with torch.no_grad():
@@ -25,7 +78,16 @@ def generate_images(model, latent_dimension, num_samples, device):
     return images
 
 
-def tensor_to_PIL_image(img_tensor):
+def tensor_to_pil_image(img_tensor):
+    """
+    Convert a PyTorch tensor to a PIL Image.
+    
+    Parameters:
+    - img_tensor (torch.Tensor): A tensor representing an image.
+
+    Returns:
+    - PIL.Image: The converted image.
+    """
     img_array = img_tensor.clone().detach().cpu().numpy()
     img_array = img_array.transpose(1, 2, 0)
     img_array = (img_array * 255).round().astype(np.uint8)
@@ -33,7 +95,15 @@ def tensor_to_PIL_image(img_tensor):
 
 
 def main(params, path_data, path_images_generated):
+    """
+    Main function to load a pre-trained generator model, produce images, and save them.
     
+    Parameters:
+    - params (dict): A dictionary of parameters containing model and generation settings.
+    - path_data (str): The path to the directory containing the model checkpoint.
+    - path_images_generated (str): The path to the directory where the generated images 
+    will be saved.
+    """
     num_samples = params['num_samples']
     output_directory = os.path.join(path_images_generated, params["training_version"])
 
@@ -44,7 +114,9 @@ def main(params, path_data, path_images_generated):
 
     checkpoint = torch.load(checkpoint_path)
 
-    generator = Generator(params["z_dim"], params["channels_img"], params["features_g"], img_size=params['image_size']).to(device)
+    generator = Generator(
+        params["z_dim"], params["channels_img"], params["features_g"], img_size=params['image_size']
+    ).to(device)
 
     generator.load_state_dict(checkpoint['generator_state_dict'])
     generator.eval()
@@ -57,14 +129,14 @@ def main(params, path_data, path_images_generated):
 
     os.makedirs(output_directory, exist_ok=True)
 
-    upscale_width = params.get('upscale_width')
+    upscale_width = params.get('upscale')
+    image_size_str = f"{params['image_size']}x{params['image_size']}"
 
     print("Saving individual images...")
     for i in tqdm(range(num_samples)):
         individual_img = images[i].cpu().clamp(0, 1)
-        img = tensor_to_PIL_image(individual_img)
-        image_size_str = f"{params['image_size']}x{params['image_size']}"
-        
+        img = tensor_to_pil_image(individual_img)
+
         if upscale_width:
             image_size_str = f"{upscale_width}x{upscale_width}"
             img = np.asarray(img)
@@ -76,7 +148,7 @@ def main(params, path_data, path_images_generated):
 
     print("Saving image grid...")
     grid_img = vutils.make_grid(images, nrow=int(num_samples**0.5), padding=2, normalize=True)
-    img_grid = tensor_to_PIL_image(grid_img.cpu())
+    img_grid = tensor_to_pil_image(grid_img.cpu())
 
     if upscale_width:
         img_grid = np.asarray(img_grid)
